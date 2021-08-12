29d8b1ee2c7b00383220c7b48ccade4eebb16d98
asolis04 <anita4.7@hotmail.com>
2021-08-11 18:09:51 -0600

    Cambios Registrar - Login

--- js/davak-inicio.js
+++ js/davak-inicio.js
@@ -1,3 +1,4 @@
+'use strict'
 /*Mostrar cuando la pantalla es menor a 700 el menú*/
 document.querySelector('.menu-btn').addEventListener('click', () => {
     document.querySelector('.nav-menu').classList.toggle('show')
